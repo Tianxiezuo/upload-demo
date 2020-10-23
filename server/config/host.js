0a3a8cc20480b8274e6ecbdcb2563d5b5ed6b799
gavinning <gavinning@qq.com>
2020-10-24 02:49:47 +0800

    update env config

--- server/config/host.js
+++ server/config/host.js
@@ -1,10 +1,16 @@
 const appEnv = require('../helper/appEnv')
 
+const port = appEnv.get({
+    dev: 3000,
+    production: 7009,
+})
+
 const server = appEnv.get({
     dev: 'http://127.0.0.1:3000',
     production: 'http://222.29.227.28:7009',
 })
 
 module.exports = {
+    port,
     server
 }
